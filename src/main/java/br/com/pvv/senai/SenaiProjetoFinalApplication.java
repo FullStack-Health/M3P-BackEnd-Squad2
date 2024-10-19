b37490c79d2c2cb6b22217f1080b2aa822514dcf
Plinio Victor <pliniopvv@gmail.com>
2024-10-19 14:48:24 -0300

    cors allow all origins

--- src/main/java/br/com/pvv/senai/SenaiProjetoFinalApplication.java
+++ src/main/java/br/com/pvv/senai/SenaiProjetoFinalApplication.java
@@ -2,12 +2,25 @@ package br.com.pvv.senai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SenaiProjetoFinalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SenaiProjetoFinalApplication.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
 	}
 
 }
