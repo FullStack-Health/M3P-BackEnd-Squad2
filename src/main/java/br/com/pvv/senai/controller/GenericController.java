73c4f500c4d1fda5f063ade14a70e6a3295bcde1
Felipe Quérette <felipe.querette@gmail.com>
2024-10-21 20:03:38 -0300

    usuarios get by id controller outline

--- src/main/java/br/com/pvv/senai/controller/GenericController.java
+++ src/main/java/br/com/pvv/senai/controller/GenericController.java
@@ -1,25 +1,17 @@
 package br.com.pvv.senai.controller;
 
-import java.util.Map;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import br.com.pvv.senai.controller.filter.IFilter;
 import br.com.pvv.senai.entity.IEntity;
 import br.com.pvv.senai.exceptions.DtoToEntityException;
 import br.com.pvv.senai.model.dto.GenericDto;
 import br.com.pvv.senai.service.GenericService;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 public abstract class GenericController<U extends GenericDto<T>, T extends IEntity> {
@@ -46,7 +38,7 @@ public abstract class GenericController<U extends GenericDto<T>, T extends IEnti
 	}
 
 	@GetMapping("{id}")
-	public ResponseEntity get(@PathVariable long id) {
+	public ResponseEntity get(@PathVariable Long id) {
 		var retorno = getService().get(id);
 		if (retorno == null)
 			return ResponseEntity.notFound().build();
