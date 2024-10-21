19200dafdeee6b58e6d3a1f628e7987da7dabd9f
Felipe Quérette <felipe.querette@gmail.com>
2024-10-21 20:04:23 -0300

    exceptions extend runtimeException

--- src/main/java/br/com/pvv/senai/exceptions/NotAuthorizedException.java
+++ src/main/java/br/com/pvv/senai/exceptions/NotAuthorizedException.java
@@ -1,6 +1,6 @@
 package br.com.pvv.senai.exceptions;
 
-public class NotAuthorizedException extends Exception {
+public class NotAuthorizedException extends RuntimeException {
 	public NotAuthorizedException() {
 		super("Não autorizado por requisições de projeto.");
 	}
