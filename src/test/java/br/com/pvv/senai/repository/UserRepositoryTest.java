93d9d2ba3eeeb5bc3c6cc67ccee9133f13ff147f
Felipe Quérette <felipe.querette@gmail.com>
2024-10-26 00:55:06 -0300

    UserRepository tests setup (not working - table issues)

--- src/test/java/br/com/pvv/senai/repository/UserRepositoryTest.java
+++ src/test/java/br/com/pvv/senai/repository/UserRepositoryTest.java
@@ -1,4 +1,76 @@
+package br.com.pvv.senai.repository;
+
+import br.com.pvv.senai.entity.Usuario;
+import br.com.pvv.senai.enums.Perfil;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Example;
+import org.springframework.test.context.ActiveProfiles;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class UserRepositoryTest {
-  
+
+    @Autowired
+    UserRepository userRepository;
+
+    Usuario usuario = new Usuario();
+
+    @BeforeEach
+    void setup(){
+        usuario.setId(1L);
+        usuario.setEmail("usuario@teste.com");
+        usuario.setPassword("12345678");
+        usuario.setPerfil(Perfil.ADMIN);
+    }
+
+    @Test
+    @DisplayName("Deve lançar um erro ao salvar pessoa usuária vazia no repositório")
+    void saveException(){
+        assertThrows(Exception.class, () -> userRepository.save(new Usuario()));
+    }
+
+    @Test
+    @DisplayName("Deve salvar pessoa usuária no repositório e retornar pessoa salva")
+    void save(){
+        assertEquals(userRepository.save(usuario), usuario);
+    }
+
+    @Test
+    @DisplayName("Deve verificar se uma pessoa usuária existe")
+    void exists(){
+        userRepository.saveAndFlush(usuario);
+        Example<Usuario> example = Example.of(usuario);
+        assertTrue(userRepository.exists(example));
+    }
+
+    //TODO findOne
+
+    @Test
+    @DisplayName("Deve retornar uma lista de todas pessoas usuárias")
+    void findAll() {
+        userRepository.saveAndFlush(usuario);
+        assertTrue(userRepository.findAll().size() > 0);
+    }
+
+    @Test
+    @DisplayName("Deve retornar uma pessoa usuária através do Id")
+    void findById() {
+        userRepository.saveAndFlush(usuario);
+        assertEquals(userRepository.findById(1L), usuario);
+    }
+
+
+
+    //TODO existsById
+
+    //TODO delete
+
 }
--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,1 +1,10 @@
---INSERT INTO "usuario" ("perfil", "data_nascimento", "cpf", "email", "nome", "password") VALUES ('1', '2000-08-12', '12345678910', 'teste@teste.com', 'Usuário de Testes', '$2a$12$kn9RMO2pcOCYONZcQES2ROLfduIPWsHBnPNF1eyDacCR1VegYs7ku');
+CREATE TABLE IF NOT EXISTS "usuario" (
+    "id" BIGINT PRIMARY KEY,
+    "perfil" VARCHAR(255),
+    "data_nascimento" DATE,
+    "cpf" VARCHAR(11),
+    "email" VARCHAR(255),
+    "nome" VARCHAR(255),
+    "password" VARCHAR(255)
+);
+INSERT INTO "usuario" ("perfil", "data_nascimento", "cpf", "email", "nome", "password") VALUES ('1', '2000-08-12', '12345678910', 'teste@teste.com', 'Usuário de Testes', '$2a$12$kn9RMO2pcOCYONZcQES2ROLfduIPWsHBnPNF1eyDacCR1VegYs7ku');
