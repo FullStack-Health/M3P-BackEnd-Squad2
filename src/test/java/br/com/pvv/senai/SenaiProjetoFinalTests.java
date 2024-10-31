97e913425528bb6017609140d956c108382fa3ce
Plinio Victor <pliniopvv@gmail.com>
2024-10-31 20:25:32 -0300

    fix: tests don't works

---
+++ src/test/java/br/com/pvv/senai/SenaiProjetoFinalTests.java
@@ -0,0 +1,13 @@
+package br.com.pvv.senai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SenaiProjetoFinalTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
