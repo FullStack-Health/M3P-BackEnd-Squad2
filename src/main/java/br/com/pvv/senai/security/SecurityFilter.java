38a1398c3565ec2aa26cc8a556ee041d1601621c
Felipe Quérette <felipe.querette@gmail.com>
2024-11-03 20:02:44 -0300

    cleanup securityfilter

--- src/main/java/br/com/pvv/senai/security/SecurityFilter.java
+++ src/main/java/br/com/pvv/senai/security/SecurityFilter.java
@@ -34,28 +34,6 @@ public class SecurityFilter extends OncePerRequestFilter {
 
 			var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
 			SecurityContextHolder.getContext().setAuthentication(authentication);
-
-//			var token = this.recoverToken(request);
-//			if (token != null) {
-//				var subject = tokenService.validateToken(token);
-//				Usuario user = repository.findByEmail(subject).orElse(null);
-//
-//				// Validação para garantir que o usuário só possa acessar seus próprios dados
-//				String requestUri = request.getRequestURI();
-//				String[] uriParts = requestUri.split("/");
-//				if (uriParts.length > 2 && "pacientes".equals(uriParts[1])) {
-//					Long idFromUri = Long.valueOf(uriParts[2]); // A id do URI deve ser convertida para Long
-//
-//					// Comparação direta com o valor primitivo
-//					if (user.getPaciente().getId() != idFromUri) { // Usando != para comparação de primitivas
-//						response.setStatus(HttpStatus.FORBIDDEN.value());
-//						return; // Bloqueia o acesso
-//					}
-//				}
-//
-//				var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
-//				SecurityContextHolder.getContext().setAuthentication(authentication);
-
 		}
 		filterChain.doFilter(request, response);
 	}
